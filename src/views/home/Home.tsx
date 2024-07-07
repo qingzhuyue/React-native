fda89460613064ace2821caf1da1e4cf93693a78
qingzhuyue <qingzhuyue@foxmail.com>
2024-07-07 10:40:15 +0800

    路由

---
+++ src/views/home/Home.tsx
@@ -0,0 +1,22 @@
+/*
+ * @Author: qingzhuyue qingzhuyue@foxmail.com
+ * @Date: 2024-07-05 13:49:44
+ * @LastEditors: qingzhuyue qingzhuyue@foxmail.com
+ * @LastEditTime: 2024-07-05 13:49:45
+ * @FilePath: /RN/src/views/home/Home.tsx
+ * @Description: 
+ * Copyright (c) 2024 by ${qingzhuyue} email: ${qingzhuyue@foxmail.com}, All Rights Reserved.
+ */
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+
+export default HomeScreen;
